14ea5401eb725d2ca6a03dd759040141b0d721a7
looonnng <122837733+looonnng@users.noreply.github.com>
2025-05-28 17:14:48 -0500

    feat: handle delete guitar

--- controllers/guitarController.js
+++ controllers/guitarController.js
@@ -1,10 +1,24 @@
 const db = require('../db/queries');
+require('dotenv').config();
 
 const brandProductsGet = async (req, res) => {
   const brand = req.params.brand;
   const brandGuitars = await db.getAllBrandProducts(brand);
 
   res.render('products', { title: brand.toUpperCase(), guitars: brandGuitars });
+};
+
+const guitarDelete = async (req, res) => {
+  const brand = req.params.brand;
+  const guitarName = req.params.guitarName;
+  const userInput = req.body.confirmPassword;
+
+  if (userInput === process.env.PGPASSWORD) {
+    await db.deleteGuitar(brand, guitarName);
+    res.redirect('/');
+  } else {
+    res.redirect(req.originalUrl);
+  }
 };
 
 const guitarGet = async (req, res) => {
@@ -21,4 +35,5 @@ const guitarGet = async (req, res) => {
 module.exports = {
   brandProductsGet,
   guitarGet,
+  guitarDelete,
 };
